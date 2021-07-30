c8fd49794c9473261cc2c9b9fe8e6817b4ebaaf2
Tyler Long <tyler4long@gmail.com>
2021-07-30 11:10:55 -0700

    First working version

---
+++ README.md
@@ -0,0 +1,1 @@
+# RingCentral Code Snippet Demo
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: 'https://platform.ringcentral.com',
+});
+
+rc.token = {access_token: process.env.RINGCENTRAL_ACCESS_TOKEN};
+
+(async () => {
+  const post = await rc
+    .restapi()
+    .glip()
+    .chats(process.env.RINGCENTRAL_GROUP_ID)
+    .posts(process.env.RINGCENTRAL_POST_ID)
+    .get();
+  console.log(JSON.stringify(post, null, 2));
+})();
