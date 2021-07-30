b416b6663cd1247d8da5b0e8d6297ad7adbab26a
Tyler Long <tyler4long@gmail.com>
2021-07-30 11:12:13 -0700

    Minor change

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import RingCentral from '@rc-ex/core';
 
 const rc = new RingCentral({
-  server: 'https://platform.ringcentral.com',
+  server: process.env.RINGCENTRAL_SERVER_URL,
 });
 
 rc.token = {access_token: process.env.RINGCENTRAL_ACCESS_TOKEN};
